8f2ab6181a869e4842295b1208cb22b0c20ba190
muhammad-najib <72295906+muhammad-najib@users.noreply.github.com>
2021-06-01 21:53:22 +0700

    Add files via upload

---
+++ sprint2/ProsedurPelaporan.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Prosedur Pelaporan</title>
+        <link rel="stylesheet" type="text/css" href="ProsedurPelaporancss.css">
+    </head>
+    <body>
+        <div class="proc">
+            <img src="http://1.bp.blogspot.com/-q7gvbJopJ08/T_0QhKkn-HI/AAAAAAAAAXU/tsn3rERff8I/s1600/Logo+Kota+Banda+Aceh.png" alt="gambar icon">
+            <h3>Prosedur Pembuatan Laporan</h3>
+            <form method="post">
+                <p>1. Penduduk membuka aplikasi dan memilih menu buat laporan</p>
+                <p>2. Setelah memilih menu buat laporan, penduduk diminta memasukkan foto bukti kejadian</p>
+                <p>3. Penduduk diminta memasukkan detail kejadian, seperti waktu, lokasi, dan deskripsi kejadian</p>
+                <p>4. Penduduk mengisikan biodata diri *</p>
+                <p>5. Penduduk menunggu hingga pegawai keuchik datang ke lokasi kejadian dan membantu mengamankan pelaku</p>
+                <br>
+                <br>
+                <h5 style="color: red; font-style: italic;">*Note : Biodata pelapor hanya untuk memastikan apakah laporan ini benar adanya dan pelapor bisa mempertanggung jawabkan laporannya</h5> 
+            </form>
+            <a  href="index.php"><button class="kembali-btn">Back</button></a>
+        </div>
+        </script>
+    </body>
+</html>
